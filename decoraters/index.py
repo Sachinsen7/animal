0d0ce95eb7f3b64b02b6b7c2d54a7e234a67fd4f
Sachin Sen <sachinsen1920@gmail.com>
2025-09-01 06:32:17 +0530

    add another examples

--- decoraters/index.py
+++ decoraters/index.py
@@ -19,31 +19,6 @@ exa_func(3)
 
 
 
-# dubugging function calls
-
-
-def debug(func):
-    def wrapper(*args, **kwargs):
-        args_value = ', '.join(str(arg) for arg in args)
-        # reverse loop
-        kwargs_value = ', '.join(f"{k}={v}" for k, v in kwargs.items())
-        print(f"calling: {func.__name__} with args {args_value} and kwargs {kwargs_value}")
-        return func(*args, **kwargs)    
-    return wrapper
-
-@debug
-def hello():
-    print("good morning")
-
-
-@debug
-def greeting(name, greeting = "Hello"):
-    print(f"{greeting}, {name}")
-
-
-hello()
-greeting("Sachin", greeting="Hey")
-
 
 
 #  cache return value => most used
